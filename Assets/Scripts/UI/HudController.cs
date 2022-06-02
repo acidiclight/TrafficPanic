7b16179325f6583d57d37e1e8cdc30c29fa84dba
Alkaline Thunder <alkalinethunder@gmail.com>
2022-06-02 14:38:27 -0400

    [enhancement] High Score display in the HUD

--- Assets/Scripts/UI/HudController.cs
+++ Assets/Scripts/UI/HudController.cs
@@ -11,6 +11,8 @@ namespace UI
     public class HudController : MonoBehaviour
     {
         private Coroutine flashCoroutine;
+        private Coroutine hsFlashCoroutine;
+        
         
         [Header("dependencies")]
         [SerializeField]
@@ -33,26 +35,46 @@ namespace UI
         [SerializeField]
         private TextMeshProUGUI scoreText;
 
+        [SerializeField]
+        private TextMeshProUGUI highScoreText;
+
         private void Awake()
         {
             Assert.IsNotNull(gameState);
             Assert.IsNotNull(player);
             Assert.IsNotNull(trafficLightPrefab);
             Assert.IsNotNull(scoreText);
+            Assert.IsNotNull(highScoreText);
         }
 
         private void OnEnable()
         {
             this.scoreText.text = $"Score: {gameState.Value.Score}";
             scoreText.color = Color.white;
+            
+            this.highScoreText.text = $"High Score: {gameState.Value.HighScore}";
+            highScoreText.color = Color.white;
+
             gameState.Value.ScoreUpdated += UpdateScore;
+            gameState.Value.HighScoreUpdated += OnHighScoreUpdated;
             gameState.Value.CurrentStateChanged += HandleCurrentStateChanged;
+        }
+
+        private void OnHighScoreUpdated(long newHighScore)
+        {
+            highScoreText.text = $"High Score: {newHighScore}";
+            
+            if (hsFlashCoroutine != null)
+                StopCoroutine(hsFlashCoroutine);
+
+            hsFlashCoroutine = StartCoroutine(FlashHighScore());
         }
 
         private void OnDisable()
         {
             gameState.Value.ScoreUpdated -= UpdateScore;
             gameState.Value.CurrentStateChanged -= HandleCurrentStateChanged;
+            gameState.Value.HighScoreUpdated -= OnHighScoreUpdated;
         }
 
         private void HandleCurrentStateChanged(CurrentGameState newState)
@@ -90,6 +112,23 @@ namespace UI
             this.scoreText.color = Color.white;
             this.flashCoroutine = null;
         }
+        
+        private IEnumerator FlashHighScore()
+        {
+            var color = this.positiveScoreColor;
+
+            for (var i = 0; i <= 15; i++)
+            {
+                var alpha = (float)(i % 2);
+                this.highScoreText.color = color * alpha;
+
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            this.highScoreText.color = Color.white;
+            this.hsFlashCoroutine = null;
+        }
+
 
         public TrafficLightController SpawnTrafficLightHud(Transform levelArea)
         {
